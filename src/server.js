ad6364e47ca048089016b5f81d56c1fc2f4bdc3a
BrLacerda <113605155+BrLacerda@users.noreply.github.com>
2023-02-14 20:14:43 -0300

    Update server.js
    test4

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ import Produto from "./Models/produtos.js";
 
 app.set("port", process.env.PORT || 3000)
 
-        // Rotas
+            // Rotas 
+    // Rotas Get
+
 app.get('/', (req, res) => {
     res.sendStatus(200)
 });
@@ -18,10 +20,12 @@ app.get('/produtos', async (req, res, next) => {
         return res.json(produtos)
     }).catch((err) => {
         return res.status(400).json({
-            erro: err,
+            erro: err.name,
             message: "falha na requisição"});
     })
 });
+
+    // Rotas Post
 
 app.post('/cadastrar', async (req, res, next) => {
     await User.create(req.body)
@@ -49,9 +53,14 @@ app.post('/estocar', async (req, res, next) => {
     })
 });
 
+    // Rotas Put
+
 app.put('/', (req, res, next) => {
+
     res.send('PUT')
 });
+
+    // Rotas Delete
 
 app.delete('/', (req, res, next) => {
     res.send('DELETE')
@@ -60,5 +69,5 @@ app.delete('/', (req, res, next) => {
 
         // Servidor rodando em: 
 app.listen(app.get("port"), () => {   
-    console.log(`Listening on port ${app.get("port")}`)
+    console.log(`Listening on port localhost:${app.get("port")}`)
 })
