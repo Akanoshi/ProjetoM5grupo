7d9cd7e6943402af2257285469d2eb44643eb6e5
Akanoshi <113038068+Akanoshi@users.noreply.github.com>
2023-02-14 00:51:02 -0300

    Base do projeto

---
+++ index.jsx
@@ -0,0 +1,20 @@
+// config inicial
+const express = require('express')
+const app = express()
+
+// forma de ler JSON /middlewares
+app.use(
+      express.urlencoded({
+            extended: true,
+      }),
+)
+
+app.use(express.json())
+
+// rota inicial / endpoint
+app.get('/', (req,res) => {
+
+})
+
+//entregar uma porta
+app.listen(3000)
