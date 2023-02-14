8bc4a9f94f929022cac22618042332fde7f5d922
BrLacerda <113605155+BrLacerda@users.noreply.github.com>
2023-02-14 07:35:51 -0300

    Add files via upload
    add src

---
+++ src/Models/produtos.js
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+import db from "../infra/db.js";
+
+const Produto = db.define("produtos" , {
+    id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    descricao: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    valor: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+    },
+    img: {
+        type: Sequelize.BLOB,
+        allowNull: true,
+    }
+})
+
+Produto.sync({alter: true})
+
+export default Produto;
---
+++ src/server.js
@@ -0,0 +1,53 @@
+import app from "./app.js";
+import User from "./Models/user.js";
+import Produto from "./Models/produtos.js";
+const port = 3000
+
+
+
+app.get('/produtos', async (req, res, next) => {
+
+    await Produto.findAll()
+    .then((produtos) => {
+        console.log(produtos)
+        return res.json({
+            erro: false,
+            message: "sucesso"
+        })
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: err,
+            message: "falha no cadastro"});
+    })
+});
+
+app.post('/produtos', async (req, res, next) => {
+    console.log(req.body)
+
+    await Produto.create(req.body)
+    .then(()=> {
+        return res.json({
+            erro: false,
+            message: "sucesso"});
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: err,
+            message: "falha no cadastro"});
+    })
+
+    res.send('POST')
+});
+
+app.put('/', (req, res, next) => {
+    res.send('PUT')
+});
+
+app.delete('/', (req, res, next) => {
+    res.send('DELETE')
+});
+
+
+
+app.listen(port, () => {   
+    console.log(`Listening on port ${port}`)
+})
