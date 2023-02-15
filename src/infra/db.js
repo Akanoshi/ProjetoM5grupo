92104355091b59ab6518c7871737d62c3d1d34a8
Brlacerda <brun0lac3rd4@gmail.com>
2023-02-14 21:17:19 -0300

    21:17

--- src/infra/db.js
+++ src/infra/db.js
@@ -2,6 +2,8 @@ import Sequelize from "sequelize";
 
 const db = new Sequelize('hermes_corporation', 'root', 'root', {
     host: 'localhost',
+    port: 3306,
     dialect: 'mysql',
 });
+
 export default db;
