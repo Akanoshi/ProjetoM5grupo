7b20fa5f690e4f8c4b9d30ca7ec8d533966f111b
marceloydm <marceloydm@gmail.com>
2023-02-15 05:54:12 -0300

    script js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
 async function getContent() {
     try {
-        const response = await fetch('http://localhost:3000/')
-        // const data = await response.json()
+        const response = await fetch('http://localhost:3000/produtos')
+        const data = await response.json()
 
-        // console.log(data)
+        console.log(data)
         console.log(response)
+        show(data)
     } catch (error) {
         console.error(error)
     }
 }
     getContent()
+
+    function show(data) {
+        let output = ''
+    
+        for(let nome of data) {
+            output += `<h1>${nome.name}</h1>`
+    
+            document.querySelector('.content').innerHTML = output
+    
+        }
+    }
